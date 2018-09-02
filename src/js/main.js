07083db0b7a21039df44da5f29e0d3641ba3be66
Groyreg <groyreg@gmail.com>
2018-09-02 16:59:50 +0300

    initial commit for configuration webpack

---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+import jQuery from 'jquery';
+import popper from 'popper.js';
+import bootstrap from 'bootstrap';
+require.context('../fonts', true, /\.(woff|woff2|otf|ttf|eot)$/);
+require.context('../img', true, /\.(png|jpg|jpeg|gif|ico|webp|webmanifest)$/);
+console.log('Hello Bro!');
